325dbef92f24c3c22d863ee484d998d356446a00
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-09-12 11:59:16 +0530

    Questions UI change

--- src/Components/UI-Component/QuestionsMapper.tsx
+++ src/Components/UI-Component/QuestionsMapper.tsx
@@ -6,11 +6,12 @@ type P = {
     setQuizResponse:(w : { [qId: string]: number })=>void;
 }
 const QuestionsMapper: FC<P> = ({ questions, quizResponse , setQuizResponse }) => {
-    console.log("Quiz response",quizResponse);
+    console.log("Quiz response",questions);
     return <div>
         {
             questions.map((q, i) => {
-                return <div className='bg-white border border-black m-2 p-3'>
+                return <div className='relative bg-white border border-black m-2 p-3'>
+                    <p className={' absolute right-2 rounded-md text-sm px-3 py-2  text-white '+(q.diffeculty=== "Easy" && " bg-green-600 ")+(q.diffeculty=== "Medium" && " bg-yellow-600 ")+(q.diffeculty=== "Hard" && " bg-red-600  ")  }>{q.diffeculty}</p>
                     <h2 className='text-xl sm:text-2xl md:text-3xl font-bold'>Q{i + 1} - <span>{q.text}</span></h2>
                     <div className='pl-8 sm:text-xl'>
                         {
